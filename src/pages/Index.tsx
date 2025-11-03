81201c4993658cd2f82b0a19763d52523ae1aac3
Stronger <Stronger@net.com>
2025-11-03 20:39:16 +0700

    fix errors

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,7 @@
 // Update this page (the content is just a fallback if you fail to update the page)
 
+import React from "react";
+
 const Index = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
